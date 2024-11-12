71c0c8d290618c405bb09950c193d86c71d529eb
green-threads <jakub.trumpes@gmail.com>
2024-11-12 19:34:58 +0100

    tt

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,7 +1,6 @@
-import type { PageLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params }) => {
+export async function load({ params }){
 	const API_URL = `http://nginx:80/api/frontend/paste/${params.id}`;
 
 	try {
@@ -11,12 +10,17 @@ export const load: PageLoad = async ({ params }) => {
 			let json = await res.json();
 			json['id'] = params.id;
 
+			if (json["error"] === true) {
+				console.debug("Received expected error")
+				redirect(303, '/')
+			}
+
 			return json;
 		} else {
-			throw redirect(303, '/');
+			redirect(303, '/');
 		}
 	} catch (error) {
-		console.debug('Received expected error:', error);
-		throw redirect(303, '/');
+		console.error('Error', error);
+		redirect(303, '/');
 	}
-};
+}
